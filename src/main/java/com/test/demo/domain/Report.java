a2123200c22d3683c67e7256c8fbac7bf07b2d97
Vladislav Dodonov <vdodonov@37bytes.com>
2020-07-17 22:11:25 +0500

    added date to report

--- src/main/java/com/test/demo/domain/Report.java
+++ src/main/java/com/test/demo/domain/Report.java
@@ -4,10 +4,12 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.Entity;
+import java.time.LocalDate;
 
 @Data
 @Entity(name = "report")
 @EqualsAndHashCode(callSuper = true)
 public class Report extends Base {
     private String name;
+    private LocalDate date;
 }
--- src/main/java/com/test/demo/dto/ReportDto.java
+++ src/main/java/com/test/demo/dto/ReportDto.java
@@ -1,12 +1,24 @@
 package com.test.demo.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import java.time.LocalDate;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class ReportDto extends BaseDto {
     @Schema(description = "Тема доклада")
     private String name;
+    @Schema(description = "Дата dd-MM-yyyy")
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
+    private LocalDate date;
 }
